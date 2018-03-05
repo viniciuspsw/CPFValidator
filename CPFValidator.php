dcdcdc5670dfb6bf18f6bdfaba2482a7617701e8
Vinícius Silva <viniciuspwd@hotmail.com>
2018-03-05 15:22:28 -0300

    Identation changes (4 spaces > tabs)

--- CPFValidator.php
+++ CPFValidator.php
@@ -11,105 +11,105 @@
 
 class CPFValidator
 {
-	/**
-	 * @var string $cpf
-	 */
-	private $cpf;
-	
-	/**
-	 * @var int $digits
-	 */
-	private $digits;
+    /**
+     * @var string $cpf
+     */
+    private $cpf;
 
-	/**
-	 * Construct class
-	 *
-	 * @param int $digits (default = 11)
-	 *
-	 * @return void
-	 */
-	public function __construct ($digits = 11)
-	{
-		$this->digits = $digits;
-	}
+    /**
+     * @var int $digits
+     */
+    private $digits;
 
-	/**
-	 * Check if CPF is valid, will return true if it is valid
-	 *
-	 * @param string $string
-	 *
-	 * @return boolean
-	 */
-	public function isValid ($cpf)
-	{
-		$this->cpf = $this->sanatize($cpf);
+    /**
+     * Construct class
+     *
+     * @param int $digits (default = 11)
+     *
+     * @return void
+     */
+    public function __construct ($digits = 11)
+    {
+        $this->digits = $digits;
+    }
 
-		if ($this->checkDigits() && !$this->checkSequence() && $this->checkAlgorithm()) {
-			return true;
-		}
+    /**
+     * Check if CPF is valid, will return true if it is valid
+     *
+     * @param string $string
+     *
+     * @return boolean
+     */
+    public function isValid ($cpf)
+    {
+        $this->cpf = $this->sanatize($cpf);
 
-		return false;
-	}
+        if ($this->checkDigits() && !$this->checkSequence() && $this->checkAlgorithm()) {
+            return true;
+        }
 
-	/**
-	 * Check if CPF is valid, will return true if is valid
-	 *
-	 * @return boolean
-	 */
-	private function checkAlgorithm ()
-	{
-		for ($t = 9; $t < 11; $t++) {
-			for ($d = 0, $c = 0; $c < $t; $c++) {
-				$d += $this->cpf{$c} * (($t + 1) - $c);
-			}
+        return false;
+    }
 
-			$d = ((10 * $d) % 11) % 10;
+    /**
+     * Check if CPF is valid, will return true if is valid
+     *
+     * @return boolean
+     */
+    private function checkAlgorithm ()
+    {
+        for ($t = 9; $t < 11; $t++) {
+            for ($d = 0, $c = 0; $c < $t; $c++) {
+                $d += $this->cpf{$c} * (($t + 1) - $c);
+            }
 
-			if ($this->cpf{$c} != $d) {
-				return false;
-			}
-		}
+            $d = ((10 * $d) % 11) % 10;
 
-		return true;
-	}
+            if ($this->cpf{$c} != $d) {
+                return false;
+            }
+        }
 
-	/**
-	 * Check if $cpf length is the same length of $digits, will return true if it is
-	 *
-	 * @return boolean
-	 */
-	private function checkDigits ()
-	{
-		if (strlen($this->cpf) == $this->digits) {
-			return true;
-		}
+        return true;
+    }
 
-		return false;
-	}
+    /**
+     * Check if $cpf length is the same length of $digits, will return true if it is
+     *
+     * @return boolean
+     */
+    private function checkDigits ()
+    {
+        if (strlen($this->cpf) == $this->digits) {
+            return true;
+        }
 
-	/**
-	 * Check if $cpf is a squenece of numbers, will return true if it is a sequence
-	 *
-	 * @return boolean
-	 */
-	private function checkSequence ()
-	{
-		if (preg_match('/(\d)\1{10}/', $this->cpf)) {
-			return true;
-		}
+        return false;
+    }
 
-		return false;
-	}
+    /**
+     * Check if $cpf is a squenece of numbers, will return true if it is a sequence
+     *
+     * @return boolean
+     */
+    private function checkSequence ()
+    {
+        if (preg_match('/(\d)\1{10}/', $this->cpf)) {
+            return true;
+        }
 
-	/**
-	 * Allow only numbers in string
-	 *
-	 * @param string $string
-	 *
-	 * @return string
-	 */
-	private function sanatize ($string)
-	{
-		return preg_replace('/[^0-9]/is', '', $string);
-	}
+        return false;
+    }
+
+    /**
+     * Allow only numbers in string
+     *
+     * @param string $string
+     *
+     * @return string
+     */
+    private function sanatize ($string)
+    {
+        return preg_replace('/[^0-9]/is', '', $string);
+    }
 }
