844abb4089eab9c6b7e8d654ade7ac241b1a687a
Vinícius Silva <viniciuspwd@hotmail.com>
2018-03-05 15:24:45 -0300

    Identation changes

--- sample_test.php
+++ sample_test.php
@@ -5,9 +5,9 @@ require_once 'CPFValidator.php';
 $validator = new CPFValidator;
 
 if ($validator->isValid('268.548.500-70')) {
-	echo 'The CPF is valid!';
+    echo 'The CPF is valid!';
 } else {
-	echo 'The CPF is not valid.';
+    echo 'The CPF is not valid.';
 }
 
 // Result: The CPF is valid
